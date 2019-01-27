a4ff673bd8f553a9ed20d9832f1a0996a58839eb
djoldie777 <alexman.p93@gmail.com>
2019-01-27 18:36:51 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace hopfield_network
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.recognizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.recognizeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(200, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // recognizeToolStripMenuItem
+            // 
+            this.recognizeToolStripMenuItem.Name = "recognizeToolStripMenuItem";
+            this.recognizeToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.recognizeToolStripMenuItem.Text = "Recognize";
+            this.recognizeToolStripMenuItem.Click += new System.EventHandler(this.recognizeToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(200, 224);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hopfield";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem recognizeToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace hopfield_network
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap bm;
+        private const string samplesPath = @"../../Samples/";
+        private const string filesToClassify = @"../../Distorted/";
+        private int countOfSamples, vecSize;
+        private int[][] X_marix;
+        private double[,] W_matrix;
+        private List<string> sampleNames;
+        private const int iterations = 100;
+        private const int e = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+            bm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            sampleNames = new List<string>();
+            LoadSamples();
+            TrainNetwork();
+        }
+
+        private void LoadSamples()
+        {
+            string[] files = Directory.GetFiles(samplesPath);
+            countOfSamples = files.Count();
+            X_marix = new int[countOfSamples][];
+
+            for (int i = 0; i < countOfSamples; i++)
+            {
+                Bitmap b = new Bitmap(files[i]);
+                X_marix[i] = new int[b.Width * b.Height];
+
+                sampleNames.Add(files[i].Substring(files[i].Length - 5, 1));
+
+                for (int j = 0; j < b.Height; j++)
+                    for (int k = 0; k < b.Width; k++)
+                        X_marix[i][(j * b.Width) + k] = b.GetPixel(k, j).ToArgb() == 0 ? -1 : 1;
+            }
+        }
+
+        private void TrainNetwork()
+        {
+            vecSize = X_marix[0].Length;
+            W_matrix = new double[vecSize, vecSize];
+
+            for (int i = 0; i < vecSize; i++)
+            {
+                for (int j = 0; j < vecSize; j++)
+                {
+                    if (i == j)
+                        W_matrix[i, j] = 0;
+                    else
+                    {
+                        double sum = 0;
+
+                        // Hebb learning
+                        for (int k = 0; k < countOfSamples; k++)
+                            sum += X_marix[k][i] * X_marix[k][j];
+
+                        W_matrix[i, j] = sum / vecSize;
+                    }
+                }
+            }
+        }
+
+        private int[] GetVector()
+        {
+            int[] vector = new int[vecSize];
+
+            for (int j = 0; j < bm.Height; j++)
+                for (int i = 0; i < bm.Width; i++)
+                    vector[(j * bm.Width) + i] = bm.GetPixel(i, j).ToArgb() == 0 ? -1 : 1;
+
+            return vector;
+        }
+
+        private int[] GetArrayCopy(int[] arr)
+        {
+            int[] res = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+                res[i] = arr[i];
+
+            return res;
+        }
+
+        private bool EquivalenceCheck(int[] arr1, int[] arr2)
+        {
+            if (arr1.Length != arr2.Length)
+                return false;
+
+            for (int i = 0; i < arr1.Length; i++)
+                if (arr1[i] != arr2[i])
+                    return false;
+
+            return true;
+        }
+
+        private bool ErrorGuard(int[] arr1, int[] arr2)
+        {
+            int errorCount = 0;
+
+            if (arr1.Length != arr2.Length)
+                return false;
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] != arr2[i])
+                    errorCount++;
+
+                if (errorCount > e)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private void RecognizeVector(int[] y)
+        {
+            for (int iter = 0; iter < iterations; iter++)
+            {
+                int[] oldY = GetArrayCopy(y);
+
+                for (int j = 0; j < vecSize; j++)
+                {
+                    double sum = 0;
+
+                    for (int i = 0; i < vecSize; i++)
+                        sum += W_matrix[i, j] * y[i];
+
+                    // Activation function
+                    y[j] = Math.Sign(sum);
+                }
+
+                if (EquivalenceCheck(y, oldY))
+                    break;
+            }
+
+            for (int k = 0; k < countOfSamples; k++)
+            {
+                if (ErrorGuard(y, X_marix[k]))
+                {
+                    MessageBox.Show(sampleNames[k] + " is Recognised", "Result", MessageBoxButtons.OK);
+
+                    Bitmap b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                    Graphics g = Graphics.FromImage(b);
+                    pictureBox1.Image = b;
+
+                    for (int i = 0; i < y.Length; i++)
+                    {
+                        if (y[i] == 1)
+                        {
+                            Rectangle rect = new Rectangle((i % 10) * 20, (i / 10) * 20, 20, 20);
+                            g.FillRectangle(Brushes.Black, rect);
+                        }
+                    }
+
+                    g.Dispose();
+                    pictureBox1.Invalidate();
+
+                    return;
+                }
+            }
+
+            MessageBox.Show("Unknown Letter", "Result", MessageBoxButtons.OK);
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = Path.Combine(Directory.GetParent(Directory.GetParent(
+                   Directory.GetCurrentDirectory()).ToString()).ToString(), "Distorted");
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                bm = new Bitmap(openFileDialog1.FileName);
+                Bitmap b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics g = Graphics.FromImage(b);
+                pictureBox1.Image = b;
+
+                for (int j = 0; j < bm.Height; j++)
+                {
+                    for (int i = 0; i < bm.Width; i++)
+                    {
+                        if (bm.GetPixel(i, j).ToArgb() != 0)
+                        {
+                            Rectangle rect = new Rectangle(i * 20, j * 20, 20, 20);
+                            g.FillRectangle(Brushes.Black, rect);
+                        }
+                    }
+                }
+
+                g.Dispose();
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void recognizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RecognizeVector(GetVector());
+        }
+    }
+}
